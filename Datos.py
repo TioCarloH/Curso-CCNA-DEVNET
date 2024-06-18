8112ba4b9f51f9293a2e9a4229e116024859d924
NetAcad DEVASC <changeme@example.com>
2024-06-18 20:31:25 +0000

    ET DEVNET CARLOS DIAZ

---
+++ Datos.py
@@ -0,0 +1,4 @@
+primerNombre = input ("Cual es tu nombre? ")
+Apellido = input ("Cual es tu apellido? ")
+Edad = input ("Cual es tu edad? ")
+print("Hola " +primerNombre, Apellido +"! Tienes " + Edad + "años. ")
